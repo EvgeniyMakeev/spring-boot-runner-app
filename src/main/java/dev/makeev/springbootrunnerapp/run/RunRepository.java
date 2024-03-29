fdd128a9e9cefe79d2b73ec8a9f88f02b3922cf1
Evgeniy Makeev <evgeniyvmakeev@gmail.com>
2024-03-29 12:50:48 +0300

    v3.0 on Database

--- src/main/java/dev/makeev/springbootrunnerapp/run/RunRepository.java
+++ src/main/java/dev/makeev/springbootrunnerapp/run/RunRepository.java
@@ -1,23 +1,10 @@
 package dev.makeev.springbootrunnerapp.run;
 
+import org.springframework.data.repository.ListCrudRepository;
+
 import java.util.List;
-import java.util.Optional;
 
-public interface RunRepository {
+public interface RunRepository extends ListCrudRepository<Run, Integer> {
 
-    List<Run> findAll();
-
-    Optional<Run> findById(Integer id);
-
-    void create(Run run);
-
-    void update(Run run, Integer id);
-
-    void delete(Integer id);
-
-    int count();
-
-    void saveAll(List<Run> runs);
-
-    List<Run> findByLocation(String location);
+    List<Run> findAllByLocation(String location);
 }
